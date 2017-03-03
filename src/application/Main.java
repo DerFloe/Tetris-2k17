8941e25e773f8c52a5ddfcf635e5a67d44ed5b0e
derfloe <florianhofer@localhost>
2017-03-03 10:41:42 +0100

    comment

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		try {// bsdfs
+		try {// bsdfsdsfkdahfia
 			Button btnStart=new Button();
 			btnStart.setText("Spiel starten");
 			Button btnSettings=new Button();
