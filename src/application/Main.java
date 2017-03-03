85dacbc43e5201d78bbcaf19bf4525ad77c7f67a
sebi12345 <Sebi@10.91.53.34>
2017-03-03 10:31:35 +0100

    main geaendert

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,48 @@
 package application;
-	
+
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.StackPane;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	Button button;
+	Stage window ;
 	@Override
 	public void start(Stage primaryStage) {
-		try {
+		StackPane layout = new StackPane();
+		primaryStage = window;
+		
+		
+		button= new Button("Start");
+		button.setText("Start");
+		button.setOnAction(e -> System.out.println("Started"));
+		
+		
+		
+	
+		layout.getChildren().add(button);
+		
+		window.setTitle("Tetris2k17");
+		
+		window.show();
+		
+		
+		
+		
+		
+		
+		/*try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -18,7 +51,7 @@ public class Main extends Application {
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
 	public static void main(String[] args) {
